fa497b0a55d7724ef2a1006555a930272389a608
rei1129 <freead9211@yahoo.co.jp>
2021-03-18 22:01:25 +0900

    フロント(仮)の適用

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root to: "portfolios#index"
+
+  #get 'portfolios/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
